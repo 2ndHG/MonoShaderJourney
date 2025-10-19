8aa3833992837cca1d1578caf34271dff8768290
Norphel <neckson817@gmail.com>
2025-10-19 14:27:47 +0800

    MultiLightA

---
+++ LightingPractice/Content/MultiLightA.fx
@@ -0,0 +1,66 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+static const int MAX_LIGHTS = 4;
+
+// Configs
+int LightCount = 0;
+float2 CanvasSize = float2(1, 1);
+
+float2 Position[MAX_LIGHTS]; // in pixel
+float Radii[MAX_LIGHTS];
+float InnerRatios[MAX_LIGHTS];
+float Intensities[MAX_LIGHTS];
+float4 Colors[MAX_LIGHTS];
+
+
+Texture2D SpriteTexture;
+
+sampler2D SpriteTextureSampler = sampler_state
+{
+	Texture = <SpriteTexture>;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR0;
+	float2 TextureCoordinates : TEXCOORD0;
+};
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+	float3 finalColor = float3(0.0, 0.0, 0.0);
+	// float widthHeightRatio = canvasSize.x / canvasSize.y;
+	for(int i=0; i<LightCount; ++i) {
+		// float2 lightCenterPosition = Position[i] / canvasSize;
+		// float2 uvPosition = input.TextureCoordinates / canvasSize;
+		// uvPosition.x *= widthHeightRatio;
+		// float radius = Radii[i] * widthHeightRatio;
+		
+		float2 pixelPosition = input.TextureCoordinates * CanvasSize;
+		float d = length(pixelPosition - Position[i]);
+		float radius = max(Radii[i], 1);
+
+		//d -= InnerRatios[i] * radius;
+		d = saturate((radius-d)/radius);
+		d = smoothstep(0, 1-InnerRatios[i], d);
+        // float t = saturate(1.0 - d / radius);
+		finalColor += Colors[i].rgb * d * Intensities[i];
+	}
+	return float4(finalColor, 1.0);
+}
+
+technique SpriteDrawing
+{
+	pass P0
+	{
+		PixelShader = compile PS_SHADERMODEL MainPS();
+	}
+};
--- LightingPractice/Game1.cs
+++ LightingPractice/Game1.cs
@@ -14,23 +14,14 @@ public class Game1 : Game
     // content
     private RenderTarget2D _lightMap;
     private RenderTarget2D _mainRenderTarget;
-    private Effect _lightGeneratorEffect;
-    private Effect _templateEffect;
     private WatchedAsset<Effect> _practiceShader;
-    private WatchedAsset<Effect> _lightingShader;
     private WatchedAsset<Effect> _combineShader;
     private Material _grayscaleEffect;
     private Material _multiLightShader;
-    private List<LightData> _lights = [];
+    private Material _multiLightA;
+    private MultiLightAData multiLightAData;
     private Texture2D _treeTexture;
     private Texture2D _whiteDotTexture;
-    private List<Vector2> lightUVs;
-    private List<float> lightRadii;
-    private List<Vector4> lightColors;
-    private List<float> lightIntensities;
-
-    private Color _ambientColor = Color.Aqua;
-
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -57,33 +48,28 @@ public class Game1 : Game
         _treeTexture = Content.Load<Texture2D>("Trees");
         _whiteDotTexture = Content.Load<Texture2D>("WhiteDot");
 
-        // _templateEffect = Content.Load<Effect>("TemplateShader");
         _practiceShader = Content.Watch<Effect>("PracticeShader");
-        _lightingShader = Content.Watch<Effect>("LightingShader");
         _combineShader = Content.Watch<Effect>("CombineShader");
         _grayscaleEffect = Content.WatchMaterial("TemplateShader");
         _grayscaleEffect.SetParameter("Saturation", 1);
         _multiLightShader = Content.WatchMaterial("MultiLightShader");
         _multiLightShader.SetParameter("LightMapSize", new Vector2(80, 80));
 
-        lightUVs = [new(20f, 20f), new(40f, 40f), new(30f, 65f)];
-        lightColors = [Color.Aqua.ToVector4(), Color.Honeydew.ToVector4(), Color.GreenYellow.ToVector4()];
-        lightRadii = [10, 30, 15];
-        lightIntensities = [.5f, .8f, 1];
+        MultiLightALoadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
         _practiceShader.TryRefresh(out _);
-        _lightingShader.TryRefresh(out _);
         _combineShader.TryRefresh(out _);
         _grayscaleEffect.Update();
         _multiLightShader.Update();
+        _multiLightA.Update();
 
 
         if (Keyboard.GetState().IsKeyDown(Keys.Space))
         {
-            foreach (var p in _multiLightShader.Effect.Parameters)
+            foreach (var p in _multiLightA.Effect.Parameters)
             {
                 Console.WriteLine(p.Name);
             }
@@ -106,21 +92,9 @@ public class Game1 : Game
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
         _spriteBatch.Draw(_treeTexture, new Rectangle(0, 0, 80, 80), new Rectangle(0, 0, 80, 80), Color.White);
         _spriteBatch.End();
-        // lightUVs[1] = new Vector2(mousePos.X / 400f, mousePos.Y / 400f);
-        _multiLightShader.Effect.Parameters["LightPositions"]?.SetValue([.. lightUVs]);
-        _multiLightShader.Effect.Parameters["LightRadii"]?.SetValue([.. lightRadii]);
-        _multiLightShader.Effect.Parameters["LightColors"]?.SetValue([.. lightColors]);
-        _multiLightShader.Effect.Parameters["PositionsArePixels"]?.SetValue(1);
-        _multiLightShader.Effect.Parameters["LightIntensities"]?.SetValue([..lightIntensities]);
 
-        _multiLightShader.Effect.Parameters["LightCount"]?.SetValue(3);
         // Light map
-        GraphicsDevice.SetRenderTarget(_lightMap);
-        GraphicsDevice.Clear(Color.Transparent);
-        _spriteBatch.Begin(samplerState: SamplerState.PointClamp, blendState: BlendState.NonPremultiplied, effect: _multiLightShader.Effect);
-
-        _spriteBatch.Draw(_whiteDotTexture, new Rectangle(0, 0, 80, 80), new Rectangle(0, 0, 1, 1), Color.White);
-        _spriteBatch.End();
+        MultiLightATest();
 
 
         // draw to screen
@@ -128,21 +102,57 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.Black);
 
 
+        if (Keyboard.GetState().IsKeyDown(Keys.L))
+        {
+            _combineShader.Asset.Parameters["LightMapTexture"].SetValue(_lightMap);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp, blendState: BlendState.NonPremultiplied, effect: _combineShader.Asset);
+            _spriteBatch.Draw(_mainRenderTarget, new Rectangle(800, 0, 400, 400), new Rectangle(0, 0, 80, 80), Color.White);
+            _spriteBatch.End();
+        }
+
+
+
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp, blendState: BlendState.NonPremultiplied);
         _spriteBatch.Draw(_lightMap, new Rectangle(0, 0, 400, 400), new Rectangle(0, 0, 80, 80), Color.White);
         _spriteBatch.Draw(_mainRenderTarget, new Rectangle(400, 0, 400, 400), new Rectangle(0, 0, 80, 80), Color.White);
-        _spriteBatch.End();
-
-        _combineShader.Asset.Parameters["LightMapTexture"].SetValue(_lightMap);
-        _spriteBatch.Begin(samplerState: SamplerState.PointClamp, blendState: BlendState.NonPremultiplied, effect: _combineShader.Asset);
-        _spriteBatch.Draw(_mainRenderTarget, new Rectangle(800, 0, 400, 400), new Rectangle(0, 0, 80, 80), Color.White);
         _spriteBatch.End();
 
 
         base.Draw(gameTime);
     }
 
-    const int MAX_LIGHTS = 16;
+    private void MultiLightALoadContent()
+    {
+        _multiLightA = Content.WatchMaterial("MultiLightA");
+        multiLightAData = new();
+        multiLightAData.Colors = [Color.Gold.ToVector4(), Color.White.ToVector4(), Color.Cyan.ToVector4()];
+        multiLightAData.Radii = [20, 20, 25];
+        multiLightAData.Positions = [new(20, 30), new(60, 60), new(30, 55)];
+        multiLightAData.InnerRatios = [.1f, .5f, .4f];
+        multiLightAData.Intensities = [1, 1, 1];
+        
+    }
+    private void MultiLightATest()
+    {
+        GraphicsDevice.SetRenderTarget(_lightMap);
+        GraphicsDevice.Clear(Color.Transparent);
+        Point mousePosition = Mouse.GetState().Position;
+        
+
+        _multiLightA.Effect.Parameters["CanvasSize"].SetValue(new Vector2(_lightMap.Width, _lightMap.Height));
+        _multiLightA.Effect.Parameters["Position"].SetValue([.. multiLightAData.Positions]);
+        _multiLightA.Effect.Parameters["Radii"].SetValue([.. multiLightAData.Radii]);
+        _multiLightA.Effect.Parameters["InnerRatios"].SetValue([.. multiLightAData.InnerRatios]);
+        _multiLightA.Effect.Parameters["Intensities"].SetValue([.. multiLightAData.Intensities]);
+        _multiLightA.Effect.Parameters["Colors"].SetValue([.. multiLightAData.Colors]);
+        _multiLightA.Effect.Parameters["LightCount"].SetValue(3);
+        _multiLightA.Effect.Parameters["Position"].SetValue([new Vector2(15, 15), new Vector2(60f, 60), new Vector2(mousePosition.X, mousePosition.Y)/5f]);
+        _spriteBatch.Begin(samplerState: SamplerState.PointClamp, blendState: BlendState.NonPremultiplied, effect: _multiLightA.Effect);
+
+        _spriteBatch.Draw(_whiteDotTexture, new Rectangle(0, 0, 80, 80), new Rectangle(0, 0, 1, 1), Color.White);
+        _spriteBatch.End();
+
+    }
 
     private RenderTarget2D CreateRenderTarget2D(int width, int height)
     {
@@ -157,12 +167,17 @@ public class Game1 : Game
             RenderTargetUsage.PreserveContents
         );
     }
-    public struct LightData
+    public struct MultiLightAData
     {
-        public Vector2 Position;
-        public float Radius;
-        public float Intensity;
-        public Vector4 Color;
+        public List<Vector2> Positions = [];
+        public List<float> Radii = [];
+        public List<float> InnerRatios = [];
+        public List<float> Intensities = [];
+        public List<Vector4> Colors =[];
+
+        public MultiLightAData()
+        {
+        }
     }
 
 
